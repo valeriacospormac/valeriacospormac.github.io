c726d32363aa04e2c5bdfebc0cbb13e36d318ebc
valeria <valeriacospormac@gmail.com>
2017-08-10 14:36:33 +0300

    functionalitatea la sectiunea din centru

--- aplicatieBloc/scripts.js
+++ aplicatieBloc/scripts.js
@@ -1,5 +1,5 @@
 /*left section*/
-function person(name, age) {
+function personFactory(name, age) {
 	return {
 		name: name,
 		age: age
@@ -8,21 +8,48 @@ function person(name, age) {
 
 let residents = [];
 
-document.querySelector("#register").addEventListener("click", function(event){
-	let yourName = document.querySelector("#name").value;
-	let yourAge = document.querySelector("#age").value;
-	residents.push(person(yourName, yourAge));
-	let list = document.querySelectorAll("li");
-	if(residents.length <= list.length) {
-		for(i = 0; i < document.querySelectorAll("li").length; i++) {
-			document.querySelectorAll("li")[i].innerText = residents[i].name + " cu varsta de " + residents[i].age + " ani";
-		}
-	} else {
-		alert("Nu mai exista locuri disponibile");
-	}
+document.querySelector(".residents .registration-form").addEventListener("submit", function(event){
+	event.preventDefault();
+
+	const name = event.target.querySelector(".person-name").value;
+	const age = event.target.querySelector(".person-age").value;
+	const person = personFactory(name, age);
+
+	residents.push(person);
+
+	const li = document.createElement("li");
+	li.innerText = person.name + " cu varsta de " + person.age + " ani";
+
+	const ol = document.querySelector(".residents .resident-list");
+	ol.appendChild(li);
+
+	event.target.reset();
 });
 
 /*center section*/
+document.querySelector(".apartments .create").addEventListener("click", function(event){
+	
 
+
+	if(document.querySelectorAll(".floor").length < 5) {
+		const divFloor = document.createElement("div");
+		document.querySelector(".apartment-floor").appendChild(divFloor);
+		divFloor.classList.add("floor");
+
+		const divApartment = document.createElement("div");
+		divFloor.appendChild(divApartment);
+		divApartment.classList.add("apartment");
+
+		const divApartment1 = document.createElement("div");
+		divFloor.appendChild(divApartment1);
+		divApartment1.classList.add("apartment");
+	} else {
+		alert("Nu mai sunt locuri disponibile pentru a crea apartamente !");
+	}
+});
+
+document.querySelector(".apartments .delete").addEventListener("click", function(event){
+	document.querySelector(".floor").remove();
+});
 
 /*right section*/
